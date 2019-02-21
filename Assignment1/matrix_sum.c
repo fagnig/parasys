0bee23615c36167abffdd2dc2a65d3dd6842ced2
Nicolai Kammersgard <s143780@student.dtu.dk>
2019-02-21 16:31:26 +0100

    Added assignment 1

---
+++ Assignment1/matrix_sum.c
@@ -0,0 +1,81 @@
+/* matrix_sum.c 
+
+   02346, F19, DTU
+
+   For instructions, see Databar Exercise 2.
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+#define ROWS  (8*1024)
+#define COLS  (8*1024)
+
+long matrix[ROWS][COLS];
+
+double get_seconds() { 
+  struct timespec time;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+  return (double) ( (double) time.tv_sec + (double) time.tv_nsec/1000000000);
+}
+
+
+void fill(long m[ROWS][COLS]) {
+  int i,j;
+
+  for (i=0; i < ROWS; i++) {
+      for (j=0; j < COLS; j++) {
+        m[i][j] = i+j; 
+      }
+  }
+}
+
+long sum1(long m[ROWS][COLS]) {
+  long register sum = 0;
+  int i,j;
+
+  for (i=0; i < ROWS; i++) {
+    for (j=0; j < COLS; j++) {
+      sum += m[i][j];
+    }
+  }
+  return sum;
+}
+
+long sum2(long m[ROWS][COLS]) {
+  long register sum = 0;
+  int i,j;
+
+  for (j=0; j < COLS; j++) {
+    for (i=0; i < ROWS; i++) {
+      sum += m[i][j];
+    }
+  }
+  
+  return sum;
+}
+
+
+int main() {
+
+  int sum;
+  double start, end;
+
+  fill(matrix);
+
+  printf("Matrix size: %dk x %dk = %dM\n", 
+	 ROWS/1024, COLS/1024, ROWS*COLS/(1024*1024));
+
+  start = get_seconds();
+  sum = sum1(matrix);
+  end = get_seconds();
+
+  printf("sum1(matrix) = %ld using %1.6fs\n", sum, end - start);
+
+  start = get_seconds();
+  sum = sum2(matrix);
+  end = get_seconds();
+
+  printf("sum2(matrix) = %ld using %1.6fs\n", sum, end - start);
+
+}
---
+++ Assignment1/timing.c
@@ -0,0 +1,117 @@
+/*  timing.c
+
+02346, F19, DTU
+
+See Databar Exercise 1 for instructions.
+
+*/
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define LIMIT_I 1000
+#define LIMIT_J 1000
+
+ 
+double get_current_time_seconds1() { 
+    /* Get current time using gettimeofday */
+	struct timeval time;
+	gettimeofday(&time, NULL);
+    return (double) ((double) time.tv_sec + (double) time.tv_usec/1000000);
+}
+
+double get_current_time_seconds2() { 
+    /* Get current time using clock_gettime using CLOCK_REALTIME */
+	struct timespec time;
+	clock_gettime(CLOCK_REALTIME, &time);
+    return (double) ( (double) time.tv_sec + (double) time.tv_nsec/1000000000);
+}
+
+double get_current_time_seconds3() { 
+    /* Get current time using clock_gettime using CLOCK_MONOTONIC */
+	struct timespec time;
+	clock_gettime(CLOCK_MONOTONIC, &time);
+    return (double) ( (double) time.tv_sec + (double) time.tv_nsec/1000000000);
+}
+
+int main() {
+    int i, j;
+    int x = 0;
+    double start1, end1; /* timing variables [seconds] */
+    double start2, end2; /* timing variables [seconds] */
+    double start3, end3; /* timing variables [seconds] */
+	
+    start1 = get_current_time_seconds1();
+    for (i = 0; i < LIMIT_I; i++) {
+        for (j = 0; j < LIMIT_J; j++) {
+            x = x + i*j;
+        }
+    }
+    end1 = get_current_time_seconds1();
+
+    start2 = get_current_time_seconds2();
+    for (i = 0; i < LIMIT_I; i++) {
+        for (j = 0; j < LIMIT_J; j++) {
+            x = x + i*j;
+        }
+    }
+    end2 = get_current_time_seconds2();
+
+    start3 = get_current_time_seconds3();	
+    for (i = 0; i < LIMIT_I; i++) {
+        for (j = 0; j < LIMIT_J; j++) {
+            x = x + i*j;
+        }
+    }
+    end3 = get_current_time_seconds3();
+    
+	//Print results
+    printf("%s\n", "1000*1000 operations.");
+    printf("Elapsed time: %1.50f sec\n"
+           "              %1.50f sec\n"
+           "              %1.50f sec\n\n", 
+           end1-start1, end2-start2, end3-start3);
+
+    //HIGHER LIMIT
+    start1 = get_current_time_seconds1();
+    for (i = 0; i < LIMIT_I*10; i++) {
+        for (j = 0; j < LIMIT_J*10; j++) {
+            x = x + i*j;
+        }
+    }
+    end1 = get_current_time_seconds1();
+
+    start2 = get_current_time_seconds2();
+    for (i = 0; i < LIMIT_I*10; i++) {
+        for (j = 0; j < LIMIT_J*10; j++) {
+            x = x + i*j;
+        }
+    }
+    end2 = get_current_time_seconds2();
+
+    start3 = get_current_time_seconds3();
+    for (i = 0; i < LIMIT_I*10; i++) {
+        for (j = 0; j < LIMIT_J*10; j++) {
+            x = x + i*j;
+        }
+    }
+    end3 = get_current_time_seconds3();
+
+	//Print Results
+    printf("%s\n", "10000*10000 operations.");
+    printf("Elapsed time: %1.50f sec\n"
+           "              %1.50f sec\n"
+           "              %1.50f sec\n\n", 
+           end1-start1, end2-start2, end3-start3);
+
+    struct timespec tis;
+    struct timespec tis2;
+
+    clock_getres(CLOCK_REALTIME, &tis2);
+    printf("%ld,%ld\n", tis2.tv_sec,tis2.tv_nsec);
+
+    clock_getres(CLOCK_MONOTONIC, &tis2);
+    printf("%ld,%ld\n", tis2.tv_sec,tis2.tv_nsec);
+}
+
