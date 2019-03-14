4933e7c62cff8c94d83475703a0787f31cf3651f
fagnig <fagnig@autistklassen.dk>
2019-03-14 15:12:23 +0100

    longest ascent

--- ls5/routempi.c
+++ ls5/routempi.c
@@ -17,27 +17,43 @@ double  lon[MAX_POS];
 double elev[MAX_POS];
 double dist[MAX_POS];
 double incr[MAX_POS];
+double gdist[MAX_POS];
+double gincr[MAX_POS];
 
 int positions = 0;
+
+int bitesize;
+
+int i,j;
 
 /* Identity info (not used in this version) */
 #define MASTER 0
 int id, procs;
 
-/* Local section positions */
 int first, last;
 double dist_to_send;
 double recvbuf;
 
+//Length
 double global_length;
 double global_max_elev, global_min_elev;
 double global_max_slope, global_min_slope;
 
+//extremes
 double local_max_elev, local_min_elev;
 double local_max_slope, local_min_slope;
 
-/* Non-local properties */
+//ascent
 double global_longest_ascent;
+
+double local_start_ascent;
+double local_mid_ascent;
+double local_end_ascent;
+
+double start_ascents[0x1000];
+double mid_ascents[0x1000];
+double end_ascents[0x1000];
+double combined_ascents[0x1000];
 
 /* Height median */
 int global_height_median;
@@ -105,19 +121,26 @@ double calc_local_dist() {
   return dist[last];
 }
 
+double calc_total_dist(int ptn) {
+  int i;
+
+  for (i = 1; i <= last; i++) {
+      gdist[i] = gdist[i-1] + incr[i];
+  }
+
+  return gdist[ptn];
+}
+
 
 void define_section() {
   /* Define section of route to be handled by this process */
 
-  int bitesize;
   bitesize = positions/procs+1;
 
   first = bitesize*id;
   last  = bitesize*(id+1);
 
   if(last >= positions) last = positions-1;
-
-  //printf("Using section %d:%d\n", first,last);
 
 }
 
@@ -163,6 +186,168 @@ void find_extremes() {
 
 }
 
+double distance_below(double height) {
+  int i;
+  double total = 0.0;
+
+  for (i = first + 1; i <= last; i++) {
+    if (elev[i-1] == elev[i] && elev[i] == height) {
+      /* flat strecth at exact height splits between above/below */
+      total += incr[i]/2;
+      continue;
+    }
+    if ((elev[i-1] < height && elev[i] > height) ||
+        (elev[i-1] > height && elev[i] < height)   ) {
+      /* strecth crossing height counts by half */
+      total += incr[i]/2;
+      continue;
+    }
+    if ((elev[i-1] <= height && elev[i] <= height) ) {
+      /* strecth fully below height  */
+      total += incr[i];
+    }
+  }
+
+  return total;
+}    
+
+double distance_above(double height) {
+  int i;
+  double total = 0.0;
+
+  for (i = first + 1; i <= last; i++) {
+    if (elev[i-1] == elev[i] && elev[i] == height) {
+      /* flat strecth at exact height splits between above/below */
+      total += incr[i]/2;
+      continue;
+    }
+    if ((elev[i] < height && elev[i-1] > height) ||
+        (elev[i] > height && elev[i-1] < height)   ) {
+      /* strecth crossing height counts by half */
+      total += incr[i]/2;
+      continue;
+    }
+    if ((elev[i] >= height && elev[i-1] >= height) ) {
+      /* strecth fully above height  */
+      total += incr[i];
+    }
+  }
+
+  return total;
+}    
+
+void print_height_median(){
+  printf("\n");
+  printf("Heigth median: %3d m\n", global_height_median);
+}
+
+void print_longest_ascent(){
+  printf("\n");
+  printf("Longest ascent is %4.0f m\n", global_longest_ascent);
+}
+
+void find_height_median_mpi() {
+  if(id == MASTER){
+    int med_l = floor(global_min_elev);      // Lower bound
+    int med_u =  ceil(global_max_elev);      // Upper bound
+
+    double diff_l =  global_length;  // All route above
+    double diff_u = -global_length;  // All route below                            
+    int med;
+
+    double global_diff;
+
+    double below, above, diff;
+
+    /* Loop invariant:  med_l <= med_u and  diff_u <= 0 <= diff_l */
+    while (med_u - med_l > 1) {
+
+    }
+
+  }
+}
+
+void find_height_median() {
+  int med_l = floor(global_min_elev);      // Lower bound
+  int med_u =  ceil(global_max_elev);      // Upper bound
+
+  double diff_l =  global_length;  // All route above
+  double diff_u = -global_length;  // All route below                            
+  int med;
+
+  double global_diff;
+
+  double below, above, diff, rdiff;
+
+  /* Loop invariant:  med_l <= med_u and  diff_u <= 0 <= diff_l */
+  while (med_u - med_l > 1) {
+    /* Make a new estimate of the median */ 
+    med = (med_l + med_u)/2;
+
+    /* Find total above and below stretchtes for local section */
+    if(first < last){
+      above = distance_above(med);
+      below = distance_below(med);
+    
+      diff = above - below;
+    }else{
+      diff = 0;
+    }
+
+    MPI_Allreduce(&diff, &rdiff, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    global_diff = rdiff;
+
+    if (global_diff == 0.0) {
+      /* Hit, just record in upper bound and break */
+      med_u = med;
+      diff_u = global_diff;
+      break;
+    }
+
+    if (global_diff > 0) {
+      med_l = med;
+      diff_l = global_diff;
+    } else {
+      med_u = med;
+      diff_u = global_diff;
+    }
+  }
+
+  /* Take the height with the smallest absolute difference */   
+  global_height_median =  (abs(diff_u) <= abs(diff_l)) ? med_u : med_l;
+}
+
+void find_longest_ascent() {
+  int i;
+  double cur_ascent = 0.0;
+
+  for (i = first+1; i < last; i++) {
+    double slope = (elev[i] - elev[i-1])/incr[i];
+    if (slope > 0.0) {
+      /* still going up */
+      cur_ascent += incr[i];
+    } else {
+      /* flat or downhill again -- record ascent */
+      if (!local_start_ascent > 0) {
+        local_start_ascent = cur_ascent + 0.0001;
+      }
+
+      if(cur_ascent > local_mid_ascent){
+        local_mid_ascent = cur_ascent;
+      }
+
+      /* new rise might start here! */
+      cur_ascent = 0.0;
+    }
+  } 
+
+  /* Do we end going up ? */
+  if (cur_ascent > 0) {
+    local_end_ascent = cur_ascent;
+  }
+}
 
 
 int main(int argc, char* args[]){
@@ -188,6 +373,13 @@ int main(int argc, char* args[]){
 
   MPI_Barrier(MPI_COMM_WORLD);
 
+  MPI_Reduce(&incr, &gincr, positions, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
+  MPI_Bcast(&gincr, positions, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+  calc_total_dist(0);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
   //DISTANCE END
 
   //EXTREMES
@@ -199,16 +391,62 @@ int main(int argc, char* args[]){
   MPI_Reduce(&local_max_slope, &global_max_slope, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
   MPI_Reduce(&local_min_slope, &global_min_slope, 1, MPI_DOUBLE, MPI_MIN, MASTER, MPI_COMM_WORLD);
 
-
   MPI_Barrier(MPI_COMM_WORLD);
 
   //EXTREMES END
 
+  //MEDIAN
 
+  //find_height_median();
+
+  //MEDIAN END
+
+  //ASCENT
+
+  find_longest_ascent();
+
+  MPI_Gather(&local_start_ascent, 1, MPI_DOUBLE,
+               &start_ascents, 1, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+  MPI_Gather(&local_mid_ascent, 1, MPI_DOUBLE,
+               &mid_ascents, 1, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+  MPI_Gather(&local_end_ascent, 1, MPI_DOUBLE,
+               &end_ascents, 1, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+  for(i = 0; i<procs+1; i++){
+    if(i==0){
+      combined_ascents[i]=start_ascents[i];
+    }
+
+    if(start_ascents[i]<1){
+      combined_ascents[i+1] = end_ascents[i];
+    }else{
+      combined_ascents[i]+=end_ascents[i]+start_ascents[i+1];
+    }
+
+  }
+
+  for(i = 0; i<procs+1; i++){
+    if(combined_ascents[i] > global_longest_ascent){
+      global_longest_ascent = combined_ascents[i];
+    }
+
+    if(mid_ascents[i] > global_longest_ascent){
+      global_longest_ascent = mid_ascents[i];
+    }
+  }
+
+
+
+  //ASCENT END
+  
   if(id == MASTER){
     print_route();
 
     print_extremes();
+
+    print_height_median();
+
+    print_longest_ascent();
   }
 
   MPI_Finalize();
