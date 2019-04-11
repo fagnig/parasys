5ca22ea3c4f2556fa906a3af4c33c2f617a867ea
fagnig <fagnig@autistklassen.dk>
2019-04-11 12:39:41 +0200

    xd

---
+++ ls10/adapt.c
@@ -0,0 +1,155 @@
+/*
+   adapt.c  Adaptive Integration
+
+   For instructions see Databar Exercise 7
+
+   Compilation: gcc -fopenmp -std=c99 -o adapt adapt.c -lm
+   
+   Usage: ./adapt <decimals> [<threads>]
+
+   Function and interval are set in main.
+ */
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+
+const double pi = 3.141592653589793238462643;
+
+long int calls = 0;
+
+/* Some functions to integrate */
+
+double f1(double x) {
+  return x;   // Integral over [0,2] = 1
+}
+
+double f2(double x) {
+  return x * x / 4.0;  // Integral over [0,2] = 5.333...
+}
+
+double f3(double x) {
+  return 4.0 / (1 + x * x);   // Integral over [0,1] = pi
+}
+
+double sin2(double x) { /* More costly way of computing sin(x) */
+  return sqrt(1 - cos(x) * cos(x));  // Integral over [0,pi] = 2 
+}
+
+double sin3(double x) { /* Even more costly way of computing sin(x) */
+  double cos =  sqrt(1 - sin2(x) * sin2(x));
+  return sqrt(1 - cos * cos);  // Integral over [0,pi] = 2 
+}
+
+/*
+  Integrate the function f(x) from a to b (a <= b) with tolerance e.
+  The tolerance e is relative, e.g. 1e-5.
+*/ 
+double integrate_seq(double a, double b, double (*f) (double), double e) {
+  calls ++;
+  double m  = (a + b) / 2;
+  double one_trap_area = (b - a) * (f(a) + f(b)) / 2;
+  double two_trap_area = (b - a) * (f(a) + f(b) + 2 * f(m))/ 4;
+
+  if (fabs(one_trap_area - two_trap_area) <= e) {
+    return two_trap_area;
+  } else {
+    double left_area, right_area;
+    left_area  = integrate_seq(a, m, f, e/2);
+    right_area = integrate_seq(m, b, f, e/2);
+    return left_area + right_area;
+  }
+}
+    
+/*
+  Parallelize using OpenMP tasks.
+*/
+double integrate_par(double a, double b, double (*f) (double), double e) {
+  calls ++;
+  double m  = (a + b) / 2;
+  double one_trap_area = (b - a) * (f(a) + f(b)) / 2;
+  double two_trap_area = (b - a) * (f(a) + f(b) + 2 * f(m))/ 4;
+
+  if (fabs(one_trap_area - two_trap_area) <= e) {
+    return two_trap_area;
+  } else {
+    double left_area, right_area;
+    left_area  = integrate_seq(a, m, f, e/2);
+    right_area = integrate_seq(m, b, f, e/2);
+    return left_area + right_area;
+  }
+}
+
+int main(int argc, char*argv[]) {
+
+  if(argc != 2 && argc != 3) {
+    printf("Usage: adapt <decimals> [<threads>]\n");
+    return 1;
+  }
+  
+  int exp = atoi(argv[1]);
+  if (exp < 1 ) {
+    printf("Exponent must be a positive number\n");
+    return 1;
+  }
+    
+  if (argc == 3) {
+    int threads = atoi(argv[2]);
+    if (threads < 1 ) {
+      printf("Number of threads must be positive\n");
+      return 1;
+    } else {
+      // Set default no. of threads
+      omp_set_num_threads(threads);
+    }
+  }
+  
+  double start_time;
+  double time_seq, time_par;
+
+  double area_seq; 
+  double area_par;
+
+  long int calls_seq = 0;
+  long int calls_par = 0;
+  
+  double a, b;
+  double e = pow(10,-exp);
+  double (*fun) (double);
+
+  /* Formulate integration problem */
+  a = 0.0;
+  b = pi;
+  fun = sin;
+  
+  printf("\nPrecision: %d, threads: %d\n\n", exp, omp_get_max_threads());
+  
+  /* Warm-up */
+  area_seq = integrate_seq(a, b, fun, e);
+
+  calls = 0;
+  start_time = omp_get_wtime();
+  area_seq = integrate_seq(a, b, fun, e);
+  time_seq = omp_get_wtime() - start_time;
+  calls_seq = calls;
+
+  calls = 0;
+  start_time = omp_get_wtime();
+  // #pragma omp parallel
+    // #pragma omp single
+      area_par = integrate_par(a, b, fun, e);
+  time_par = omp_get_wtime() - start_time;
+  calls_par = calls;
+
+  printf("Seq: Area %16.14f using %6ld calls in %2.3f s\n", area_seq, calls_seq, time_seq);
+  printf("Par: Area %16.14f using %6ld calls in %2.3f s\n", area_par, calls_par, time_par);
+
+  if (fabs(area_seq - area_par) > 2*e  ||  calls_seq != calls_par)
+    printf("\n*** ERROR ***\n");
+  printf("\nSpeedup: %2.2f\n\n", time_seq/time_par);
+
+  return 0;
+}
---
+++ ls10/nqueen.c
@@ -0,0 +1,145 @@
+/*
+   nqueen.c  N-queen solution count
+
+   For instructions see Databar Exercise 7
+
+   Compilation: gcc -fopenmp -std=c99 -o nqueen nquuen.c 
+   
+   Usage: ./nqueen <board size> [<threads>]
+*/
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+
+int size = 0;
+int sols = 0;
+
+/* Not used -- may be used for debuggin */
+void print(int queens[]) {
+  printf("Solution for size: %d\n\n", size);
+  for(int row = 0; row < size; row++) {
+    for(int col = 0; col < size; col++) {
+      if (queens[col] == row) {
+        printf("Q");
+      } else {
+        printf("-");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+/* Is it legal to place a queen at (row, col) given placements for prior cols*/
+int legal(int sol[], int row, int col) {
+  for (int c = 0; c < col; c++) {
+    if (sol[c] == row) return 0;
+    if ( abs(sol[c] - row) == col - c) return 0;
+  }
+  return 1;
+}
+
+/*
+  Given a legal placement of j queens in columns 0..j-1
+  add number of possible legal full exentions of this to
+  global count,
+ */ 
+void count_sols_seq(int sol[], int j) {
+  for (int i = 0; i < size; i++) {
+    if ( legal(sol, i, j) ) {
+      if (j == size-1) { 
+        // last column, we have a full solution
+        sols++;
+      } else {
+        // place queen in column j and keep counting
+        sol[j] = i;
+        count_sols_seq(sol, j + 1);
+      }
+    }
+  }
+}
+
+/*
+  Parallelize using OpenMP tasks.
+ */
+void count_sols_par(int sol[], int j) {
+  for (int i = 0; i < size; i++) {
+    if ( legal(sol, i, j) ) {
+      if (j == size-1) { 
+        // last column, we have a full solution
+        sols++;
+      } else {
+        // place queen in column j and keep counting
+        sol[j] = i;
+        count_sols_seq(sol, j + 1);
+      }
+    }
+  }
+}
+
+
+int main(int argc, char*argv[]) {
+
+  if(argc != 2 && argc != 3) {
+    printf("Usage: nqueen <board_size> [<threads>]\n");
+    return 1;
+  }
+  
+  size = atoi(argv[1]);
+  if(size < 1 ) {
+    printf("Board size must be positive\n");
+    return 1;
+  }
+  
+  if (argc == 3) {
+    int threads = atoi(argv[2]);
+    if(threads < 1 ) {
+      printf("Number of threads must be positive\n");
+      return 1;
+    } else {
+      // Set default no. of threads
+      omp_set_num_threads(threads);
+    }
+  }
+  
+  printf("\nBoard size: %d x %d, threads: %d\n\n", 
+         size, size, omp_get_max_threads()); 
+  
+  int queens[size];
+
+  double start_time;
+  double time_seq, time_par;
+
+  int sols_seq; 
+  int sols_par;
+
+
+  /* Warm-up */
+  sols = 0;
+  count_sols_seq(queens, 0);
+
+  sols = 0;
+  start_time = omp_get_wtime();
+  count_sols_seq(queens, 0);
+  time_seq = omp_get_wtime() - start_time;
+  sols_seq = sols;
+
+  sols = 0;
+  start_time = omp_get_wtime();
+      count_sols_par(queens, 0);
+  time_par = omp_get_wtime() - start_time;
+  sols_par = sols;
+
+  // print(queens);
+
+  printf("Seq: Found %d solutions in %2.3f s\n", sols_seq, time_seq); 
+  printf("Par: Found %d solutions in %2.3f s\n", sols_par, time_par);
+
+  if (sols_seq != sols_par) printf("\n*** ERROR ***\n\n");
+  printf("\nSpeedup: %2.2f\n\n", time_seq/time_par);
+
+  return 0;
+}
